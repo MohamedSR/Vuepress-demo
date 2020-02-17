1efa4aefa469bd523f4cc20301fde9926734dfe9
Mohamed Sallami Rhouma <ms.rhouma@comunikmail.com>
2020-02-17 10:26:16 +0100

    import project to guthub for testing

---
+++ .vuePress/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+<div>
+<h1>Test Layout</h1>
+</div></template>
+<script>
+export default {
+    name: 'HomePageLayout'
+}
+</script>
---
+++ .vuePress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: 'Comunik',
+    description: 'We love your client',
+    logo: '/comunik-logo.png',
+    themeConfig:{
+        nav: [
+            { text: "Home", link: "/" },
+            { text: "ComunikCRM", link: "/comunikCRM/" },
+            { text: "comunikVoip", link: "/comunikVoip/" },
+            { text: "comunikDashboard", link: "/comunikDashboard/" },
+            { text: "comunikSales", link: "/comunikSales/" }
+          ],
+        sidebar: 'auto'
+    }
+}
